f41df2ae3cda1bde20cbae3174d51e7a375d2b1b
SkullCrawler <69508787+SkullCrawler@users.noreply.github.com>
2023-02-06 20:02:49 +0500

    fixed shit

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -10,7 +10,7 @@ export const Contact = () => {
 	  emailjs.sendForm('service_e9d48mp', 'template_xmsu4ek', form.current, 'RHpABP1hA25fWNMEh')
 		.then((result) => {
 			console.log(result.text);
-			console.log("Message Sent!");
+			console.log("message sent!");
 			e.target.reset();
 		}, (error) => {
 			console.log(error.text);
