9e64037d535a0eea8d7f79cf75ab312111b3dd8a
SkullCrawler <69508787+SkullCrawler@users.noreply.github.com>
2023-06-26 23:21:35 +0500

    whatthefuckisthisserviceidbullshitworknowfml

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -7,7 +7,7 @@ export const Contact = () => {
 	const sendEmail = (e) => {
 	  e.preventDefault();
 	
-	  emailjs.sendForm('service_e9d48mp', 'template_o2qurmc', form.current, 'K0GxXSOplO629yB8z')
+	  emailjs.sendForm('service_a9sflbq', 'template_o2qurmc', form.current, 'K0GxXSOplO629yB8z')
 		.then((result) => {
 			console.log(result.text);
 			console.log("message sent!");
